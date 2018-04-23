e9c860da76a1125d3b0b9ace01ebbec36f83db4e
shiguangyin <shiguangyin928@gmail.com>
2018-04-23 15:49:54 +0800

    refactor base

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from Blog import views
 
+app_name = "Blog"
 urlpatterns = [
     path("article/<int:article_id>", views.blog_article),
-    path("", views.index)
+    path("", views.index, name="index")
 ]
